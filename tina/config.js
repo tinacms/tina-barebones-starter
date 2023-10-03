8b6b19e7c2200a654ec66e3bbabb186e6d2c7a77
Logan Anderson <landerson2@upei.ca>
2023-10-03 15:42:23 -0400

    update TinaCMS

--- tina/config.js
+++ tina/config.js
@@ -1,57 +1,6 @@
-import { defineConfig, defineSchema } from "tinacms";
-
-const schema = defineSchema({
-  collections: [
-    {
-      label: "Page Content",
-      name: "page",
-      path: "content/page",
-      format: "mdx",
-      fields: [
-        {
-          name: "body",
-          label: "Main Content",
-          type: "rich-text",
-          isBody: true,
-        },
-      ],
-      ui: {
-        router: ({ document }) => {
-          if (document._sys.filename === "home") {
-            return `/`;
-          }
-          return undefined;
-        },
-      },
-    },
-    {
-      label: "Blog Posts",
-      name: "post",
-      path: "content/post",
-      fields: [
-        {
-          type: "string",
-          label: "Title",
-          name: "title",
-        },
-        {
-          type: "string",
-          label: "Blog Post Body",
-          name: "body",
-          isBody: true,
-          ui: {
-            component: "textarea",
-          },
-        },
-      ],
-      ui: {
-        router: ({ document }) => {
-          return `/posts/${document._sys.filename}`;
-        },
-      },
-    },
-  ],
-});
+import { defineConfig } from "tinacms";
+import page from "./collections/page";
+import post from "./collections/post";
 
 export const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
@@ -76,7 +25,9 @@ export const config = defineConfig({
     publicFolder: "public", // The public asset folder for your framework
     outputFolder: "admin", // within the public folder
   },
-  schema,
+  schema: {
+    collections: [page, post],
+  },
 });
 
 export default config;
