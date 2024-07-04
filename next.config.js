912f3cd285244f5cfd61c31a50ccdb3d6d591356
benjaminneoh6343@gmail.com <benjaminneoh6343@gmail.com>
2024-07-04 12:09:35 +1000

    update next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
+/** @type {import('next').NextConfig} */
+
 module.exports = {
   async rewrites() {
     return [
       {
-        source: '/admin',
-        destination: '/admin/index.html',
+        source: "/",
+        destination: "/home",
       },
-    ]
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+      },
+    ];
   },
 }
