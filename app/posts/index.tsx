41003c1c47739bd35b8cb86a2aae143b267346bf
benjaminneoh6343@gmail.com <benjaminneoh6343@gmail.com>
2024-07-04 12:10:51 +1000

    migrate post list page to app route

--- app/posts/index.tsx
+++ app/posts/index.tsx
@@ -1,20 +1,13 @@
 import { Layout } from "../../components/Layout";
 import Link from "next/link";
-import { useTina } from "tinacms/dist/react";
 
 export default function PostList(props) {
-    // data passes though in production mode and data is updated to the sidebar data in edit-mode
-    const { data } = useTina({
-      query: props.query,
-      variables: props.variables,
-      data: props.data,
-    });
-    const postsList = data.postConnection.edges;
+
     return (
-      <Layout>
+      <>
         <h1>Posts</h1>
         <div>
-          {postsList.map((post) => (
+          {props.data.postConnection.edges.map((post) => (
             <div key={post.node.id}>
               <Link href={`/posts/${post.node._sys.filename}`}>
                 {post.node._sys.filename}
@@ -22,6 +15,6 @@ export default function PostList(props) {
             </div>
           ))}
         </div>
-      </Layout>
+      </>
     );
   }
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,22 +1,11 @@
-"use client";
-
 import PostList from ".";
 import { client } from "../../tina/__generated__/client";
 
-export const getPostProps = async () => {
-  const { data, query, variables } = await client.queries.postConnection();
 
-  return {
-    props: {
-      data,
-      query,
-      variables,
-      //myOtherProp: 'some-other-data',
-    },
-  };
-};
 
 export default async function Page() {
-    const { props } = await getPostProps()
-    return <PostList {...props} />
+
+  const pages = await client.queries.postConnection();
+
+  return <PostList {...pages} />
 }
