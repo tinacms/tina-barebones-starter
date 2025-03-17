eae603c0c2975be5d35940fa9ee6e0a4dd2dd649
Yumin Chen <yumin@chen.so>
2025-03-17 08:49:35 +0000

    Link Component (#9)
    Next.js's Link component wrapping around PandaCSS’s JSX css API using Box pattern-generated component.
    
    ---
    - Issue: https://git.chen.so/tina-next-ts/i/7be45a0724401d6aa8b5fb5bcd8dae3f29f893c0
    - Patch: https://git.chen.so/tina-next-ts/p/2b1d561db016340994b365b8f5f076e4ea0928fd
    - https://github.com/Chen-Software/tina-next-ts/pull/9

---
+++ app/components/Link/index.ts
@@ -0,0 +1,9 @@
+import React from "react";
+import NextLink from "next/link";
+import { Box } from "../../../styled-system/jsx";
+
+export const Link = ({ children, ...props }) => {
+  return React.createElement(Box, { as: NextLink, ...props }, children);
+};
+
+export default Link;
--- app/posts/post-list.tsx
+++ app/posts/post-list.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from "../components/Link";
 import { Box } from "../../styled-system/jsx";
 
 export default function PostList(props) {
