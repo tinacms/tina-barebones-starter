d0bf776a79687009aa7804473e6c43796b03e248
Yumin Chen <yumin@chen.so>
2025-03-26 13:22:35 +0000

    Fix Link component type errors
    - Issue: https://git.chen.so/tina-next-ts/i/7be45a0724401d6aa8b5fb5bcd8dae3f29f893c0

--- app/components/Link/index.ts
+++ app/components/Link/index.ts
@@ -1,8 +1,14 @@
-import NextLink from "next/link";
+import NextLink, { type LinkProps } from "next/link";
 import React from "react";
-import { Box } from "../../../styled-system/jsx";
+import { Box, type BoxProps } from "../../../styled-system/jsx";
 
-export const Link = ({ children, ...props }) => {
+export const Link = ({
+	children,
+	...props
+}: LinkProps &
+	BoxProps & {
+		children?: React.ReactNode;
+	}) => {
 	return React.createElement(Box, { as: NextLink, ...props }, children);
 };
 
