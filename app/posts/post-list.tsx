6471f67effcffc5201428de971e6d2608dd682cd
Yumin Chen <yumin@chen.so>
2025-03-28 18:47:23 +0000

    Add styling to PostList
    - Issue: https://git.chen.so/tina-next-ts/i/0ab47de897447d509efc5c94fb67ea32b00a9cf6
    - Patch: https://git.chen.so/tina-next-ts/p/2ccb1e75ddb41d9a060ec3aa206a0f07f5c2495b

--- app/posts/post-list.tsx
+++ app/posts/post-list.tsx
@@ -5,13 +5,54 @@ import Link from "../components/Link";
 export default function PostList({ sidebarMode = false, ...props }) {
 	return (
 		<Box as={sidebarMode ? "aside" : React.Fragment}>
-			<Box as={sidebarMode ? "div" : "h1"} fontWeight={sidebarMode && "bold"}>
+			<Box
+				as={sidebarMode ? "div" : "h1"}
+				fontWeight={sidebarMode && "bold"}
+				marginBottom={2}
+			>
 				Posts
 			</Box>
+
 			<Box>
 				{props.data.postConnection.edges.map((post) => (
-					<Box key={post.node.id}>
-						<Link href={`/posts/${post.node._sys.filename}`}>
+					<Box
+						key={post.node.id}
+						fontSize={sidebarMode && "sm"}
+						paddingLeft={sidebarMode && 2}
+						className={
+							typeof window !== "undefined" &&
+							window.location.pathname ===
+								`/posts/${post.node._sys.filename}` &&
+							"active"
+						}
+						css={{
+							"&.active": {
+								borderLeft: "2px solid token(colors.cyan.100)",
+								_osDark: {
+									borderLeft: "2px solid token(colors.cyan.900)",
+								},
+							},
+							"&.active > a": {
+								color: "token(colors.cyan.900)",
+								_osDark: {
+									color: "token(colors.cyan.100)",
+								},
+								// transition: "color 0.2s ease-in-out",
+							},
+						}}
+					>
+						<Link
+							href={`/posts/${post.node._sys.filename}`}
+							css={{
+								"&:hover": {
+									textDecoration: "underline",
+									color: "token(colors.blue.500)",
+									_osDark: {
+										color: "token(colors.blue.400)",
+									},
+								},
+							}}
+						>
 							{post.node.title}
 						</Link>
 					</Box>
