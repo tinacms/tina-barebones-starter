7d64ce5ba334ad8403edec7609c90bf516e7636d
Yumin Chen <yumin@chen.so>
2025-03-29 10:09:26 +0000

    Set up Park UI
    - Issue: https://git.chen.so/tina-next-ts/i/9fef0dcf20a408bb09ed0b662d1d87265b8c016e

--- panda.config.ts
+++ panda.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "@pandacss/dev";
+import { createPreset } from "@park-ui/panda-preset";
+import cyan from "@park-ui/panda-preset/colors/cyan";
+import neutral from "@park-ui/panda-preset/colors/neutral";
 
 export default defineConfig({
 	// Whether to use css reset
 	preflight: true,
+
+	presets: [
+		"@pandacss/preset-base",
+		createPreset({
+			accentColor: cyan,
+			grayColor: neutral,
+			radius: "sm",
+		}),
+	],
 
 	// Where to look for your css declarations
 	include: ["./app/**/*.{js,jsx,ts,tsx}"],
